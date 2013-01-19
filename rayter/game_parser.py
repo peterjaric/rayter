4fd94fa922701db7e8c5f89eab09f6851d77b384
Jonatan Heyman <jonatan@heyman.info>
2013-01-19 15:18:34 +0100

    Make GamesParser contractor's name argument optional

--- rayter/game_parser.py
+++ rayter/game_parser.py
@@ -30,7 +30,7 @@ import math
 #from time import strptime
 
 class GamesParser(object):
-    def __init__(self, file_obj, name):
+    def __init__(self, file_obj, name=None):
         self.file = file_obj
         self.score_type = 'highscore'
         self.game_name = name
