2014b7d69e468dde06bfc6d02b3b5f63543a31f1
Jonatan Heyman <jonatan@heyman.info>
2021-01-08 17:34:06 +0100

    Use setyp.cfg for PyPI metadata

--- setup.py
+++ setup.py
@@ -8,25 +8,6 @@ version = '1.0.0'
 setup(
     name='rayter',
     version=version,
-    description="Game rating command line tool",
-    long_description="""Game rating command line tool""",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Games/Entertainment",
-    ],
-    keywords='',
-    author='Peter Jaric, Jonatan Heyman',
-    author_email='peter@jaric.org, jonatan@heyman.info',
-    url='https://github.com/peterjaric/rayter',
-    license='MIT',
-    platform='Any',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    include_package_data=True,
-    zip_safe=False,
     install_requires=[],
     entry_points={
         'console_scripts': [
