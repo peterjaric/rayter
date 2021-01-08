a1ba41068df79035a8bc1330d1b7db6afe227476
Jonatan Heyman <jonatan@heyman.info>
2021-01-08 14:57:52 +0100

    Use textwrap.dedent() for nicer test code

--- tests/test_rayter.py
+++ tests/test_rayter.py
@@ -1,6 +1,7 @@
 from StringIO import StringIO
 from unittest import TestCase
 from pprint import pprint
+from textwrap import dedent
 
 from rayter.rater import Rater
 from rayter.game_parser import GamesParser
@@ -8,15 +9,23 @@ from rayter.game_parser import GamesParser
 
 class RaterTest(TestCase):
     def test_rate_game_with_negative_score(self):
-        data = \
-"""
-game_name Negative score 
+        data = dedent("""
+            game_name Negative score 
 
-game 2012-01-19 15:12
-Jonatan    1337
-Hugo 0
-Molgan     -666
-"""
+            game 2012-01-19 15:12
+            Jonatan    1337
+            Hugo 0
+            Molgan     -666
+        """)
+        parser = GamesParser(StringIO(data), "test")
+        parser.parse_file()
+        rater = Rater(parser.games)
+        rater.rate_games(parser.score_type)
+        self.assertNotEqual(
+            rater.players['Hugo'].get_rating(),
+            rater.players['Molgan'].get_rating(),
+        )
+    
         parser = GamesParser(StringIO(data), "test")
         parser.parse_file()
         rater = Rater(parser.games)
@@ -25,16 +34,15 @@ Molgan     -666
                             rater.players['Molgan'].get_rating())
 
     def test_rate_winnertakesall_game(self):
-        data = \
-"""
-game_name Winner Takes All
-score_type winnertakesall
+        data = dedent("""
+            game_name Winner Takes All
+            score_type winnertakesall
 
-game 2019-07-05 15:12
-Jonatan 0
-Hugo 0
-Molgan 1
-"""
+            game 2019-07-05 15:12
+            Jonatan 0
+            Hugo 0
+            Molgan 1
+        """)
         parser = GamesParser(StringIO(data), "test")
         parser.parse_file()
         rater = Rater(parser.games)
